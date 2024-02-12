1f91f428ac4b22b3ae8d63d13cec784f608dbb85
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-12 10:13:30 -0300

    evento nos botoes

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -7,6 +7,7 @@ class CalcController
         this._timeEl = document.querySelector('#hora')
         this._currentDate
         this.initialize()
+        this.initButtonsEvents()
     }
 
     initialize() {
@@ -14,6 +15,15 @@ class CalcController
         setInterval(() => {
             this.setDisplayDateTime()
         }, 1000)
+    }
+
+    initButtonsEvents() {
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g')
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e => {
+                console.log(btn.className.baseVal.replace('btn-', ''))
+            })
+        })
     }
 
     setDisplayDateTime() {
