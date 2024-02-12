a0c673aad4284dc09137edb61f50f3487eda3113
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-12 13:55:41 -0300

    validado o primeiro operador

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -44,22 +44,45 @@ class CalcController
         return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
     }
 
+    pushOperation(value) {
+        this._operation.push(value)
+        if (this._operation.length > 3) {
+            this.calc()
+            console.log('A', this._operation)
+        }
+    }
+
+    calc() {
+        let last = this._operation.pop()
+        let result = eval(this._operation.join(''))
+        this._operation = [
+            result, 
+            last
+        ]
+    }
+
+    setNumberToDisplay() {
+        
+    }
+
     addOperation(value) {
-        console.log('a', isNaN(this.getLastOperation()))
         if (isNaN(this.getLastOperation())) {
             if (this.isOperator(value)) {
-                this._setLastOperation(value)
+                this.setLastOperation(value)
             } else if (isNaN(value)) {
-                console.log(value)
+                console.log('B', value)
             } else {
-                this._operation.push(value)
+                this.pushOperation(value)
             }
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString()
-            this.setLastOperation(parseInt(newValue))
+            if (this.isOperator(value)) {
+                this.pushOperation(value)
+            } else {
+                let newValue = this.getLastOperation().toString() + value.toString()
+                this.setLastOperation(parseInt(newValue))
+                this.setNumberToDisplay()
+            }
         }
-
-        console.log(this._operation)
     }
 
     setError() {
