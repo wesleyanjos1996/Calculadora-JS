aa4b2ffaef39bc49cf97fb7f41b312aa40dccfd2
WESLEY DOS ANJOS <wesleyanjosprogramming@outlook.com.br>
2024-02-12 16:53:36 -0300

    Adicionando audio ao tecla

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -1,7 +1,9 @@
 class CalcController 
 {
     constructor() {
-        this._lastOperator = '+'
+        this._audio = new Audio('../../audio/click.mp3')
+        this._audioOnOff = false
+        this._lastOperator = ''
         this._lastNumber = ''
         this._operation = []
         this._locale = 'pt-BR'
@@ -37,10 +39,27 @@ class CalcController
         }, 1000)
         this.setLastNumberToDisplay()
         this.pasteFromClipboard()
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio()
+            })
+        })
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff
+    }
+
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0
+            this._audio.play()
+        }
     }
 
     initKeyboard() {
         document.addEventListener('keyup', e => {
+            this.playAudio()
             switch (e.key) {
                 case 'Escape': 
                     this.clearAll()
@@ -202,6 +221,7 @@ class CalcController
     }
 
     execBtn(value) {
+        this.playAudio()
         switch (value) {
             case 'ac': 
                 this.clearAll()
